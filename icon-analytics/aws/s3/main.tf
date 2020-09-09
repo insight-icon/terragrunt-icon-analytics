ea458d8505750d1a360dc0a5a47b95555b7e9c61
rob <robcannonxyz@gmail.com>
2020-09-09 16:29:04 -0700

    adding s3 w raw block bucket + perms

--- icon-analytics/aws/s3/main.tf
+++ icon-analytics/aws/s3/main.tf
@@ -10,3 +10,32 @@ module "block_dump" {
     enabled = true
   }
 }
+
+variable "airflow_instance_profile_name" {}
+variable "env" {}
+
+resource "aws_iam_policy" "s3_put_logs_policy" {
+  name   = "S3PutLogsPolicy${title(var.env)}"
+  policy = <<-EOT
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+          "Sid":"ReadWrite",
+          "Effect":"Allow",
+          "Action":["s3:GetObject", "s3:PutObject"],
+          "Resource":["${module.block_dump.this_s3_bucket_arn}/*"]
+        }
+    ]
+}
+EOT
+}
+
+data "aws_iam_instance_profile" "airflow" {
+  name = var.airflow_instance_profile_name
+}
+
+resource "aws_iam_role_policy_attachment" "s3_put_logs_policy" {
+  role       = data.aws_iam_instance_profile.airflow.role_name
+  policy_arn = aws_iam_policy.s3_put_logs_policy.arn
+}
--- icon-analytics/aws/s3/terragrunt.hcl
+++ icon-analytics/aws/s3/terragrunt.hcl
@@ -8,8 +8,18 @@ include {
 
 locals {
   vars = read_terragrunt_config(find_in_parent_folders("variables.hcl")).locals
+  airflow = find_in_parent_folders("airflow")
+}
+
+dependencies {
+  paths = [local.airflow]
+}
+
+dependency "airflow" {
+  config_path = local.airflow
 }
 
 inputs = {
-  block_dump_bucket = "icon-block-dump-${local.vars.env}"
+  block_dump_bucket = "icon-block-raw-${local.vars.env}"
+  airflow_instance_profile_name = dependency.airflow.outputs.instance_profile_name
 }
